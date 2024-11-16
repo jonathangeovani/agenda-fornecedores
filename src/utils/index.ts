3a85a44623df68b1a31d6be632c253146b1fab72
jonathangeovani <dongeovanimenor@gmail.com>
2024-11-16 12:43:58 -0300

    create util function to fake data

---
+++ src/utils/index.ts
@@ -0,0 +1,19 @@
+import { Blueprint as Bp } from '@cicerotcv/blueprint';
+
+const titleSchema = Bp.lorem.sentence(3).transform((s) => s.replace('.', ''));
+const descriptionSchema = Bp.lorem.sentences(5);
+const dateSchema = Bp.date.between('2024-11-15', '2024-11-30');
+
+const todoSchema = Bp.object({
+  title: titleSchema,
+  description: descriptionSchema,
+  date: dateSchema,
+});
+
+const todoCollectionSchema = Bp.array({
+  minLength: 30,
+  maxLength: 40,
+  schema: todoSchema,
+});
+
+export const todoCollection = todoCollectionSchema.compile();
